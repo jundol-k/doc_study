3176fb479a9f6e9d0664b7636a2e780f766b4110
강준 <jundol8791@gmail.com>
2020-03-22 22:50:15 +0900

    cube, 분석함수 , count over, avg over, partition by

--- sql/Script-1.sql
+++ sql/Script-1.sql
@@ -153,11 +153,74 @@ group by segment ,
 order by brand, segment;
 
 
+-- cube
+-- 지정된 grouping 컬럼의 다차원 합계를 생성하는데 사용된다.
+select brand, segment, sum(quantity)
+from sales
+group by 
+	cube (brand, segment)
+order by brand, segment;
 
 
+-- 분석함수
+-- 특정 집합 내에서 결과 건수의 변화 없이 해당 집합안에서 합계 및 카운트 등을 계산할 수 있는 함수
+
+create table product_group (
+	group_id serial primary key,
+	group_name varchar (255) not null
+)
+
+create table product (
+	product_id serial primary key,
+	product_name varchar (255) not null,
+	price decimal (11,2),
+	group_id int not null,
+	foreign key (group_id)
+	references product_group (group_id)
+);
+
+insert into product_group (group_name)
+values 
+ ('SmartPhone'),
+ ('Laptop'),
+ ('Tablet');
+
+insert into product (product_name, group_id, price)
+values
+ ('Microsoft Lumia', 1, 200),
+ ('Htc One', 1, 400),
+ ('Nexus', 1, 500),
+ ('iPhone', 1, 900),
+ ('HP Elite',2, 1200),
+ ('Lenovo Thinkpad', 2, 700),
+ ('Sony VAIO', 2, 700),
+ ('Dell Vostro', 2, 800),
+ ('iPad', 3, 700),
+ ('Kindle Fire', 3, 150),
+ ('Samsung Galaxy Tab', 3, 200);
+
+select * from product_group;
+select * from product;
+
+select COUNT(*) from product;
+-- 집계함수의 한계 > 집계함수는 집계의 결과만을 출력한다.
+
+select count(*) over(), A.* from product A;
 
 
+-- AVG 함수
+select AVG (PRICE) over(), count(*) over(), A.* from product A;
 
+select B.group_name , AVG(price) from product A
+inner join product_group B
+on A.group_id = B.group_id 
+group by B.group_name;
+
+select 
+	A.product_name, A.price, B.group_name , AVG(price) over (partition by b.group_name) 
+from product A
+inner join product_group B
+on A.group_id = B.group_id;
 
 
 
